481a7cad9c8a8ce96af18b22c78795943febf5b8
Tobias Ford <toby.ford@pobox.com>
2018-04-05 11:27:30 -0400

    initial commit based on https://anderspitman.net/2018/04/04/static-react-rust-webapp/

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# React+Rust webapp in a small binary
+
+## Based on .. https://anderspitman.net/2018/04/04/static-react-rust-webapp/
---
+++ ui/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hi there Mr Toby!</h1>,
+  document.getElementById('root')
+);
